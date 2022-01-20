58372a1ca85f8e6f79799b71ee99ba9a55a0beee
Michael-Vol <mivolakis@gmail.com>
2022-01-20 03:55:37 +0200

    added _document

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					<meta name='title' content='Michael Volakis' />
+					<meta name='description' content='My Personal Portfolio' />
+					<meta
+						name='keywords'
+						content='Michael,Volakis,DIT,Computer Science,Informatics,Athens,Portfolio,Developer,Web Developer,Web Apps'
+					/>
+					<meta name='robots' content='index, follow' />
+					<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+					<meta name='language' content='English' />
+
+					<link rel='icon' href='/vectors/favicon.png' />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
